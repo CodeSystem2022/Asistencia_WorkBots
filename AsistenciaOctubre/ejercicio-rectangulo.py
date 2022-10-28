6e321c5a3d26775fe1eb3127c56c92a12c041bcc
Vilma Jara <105697175+ivi2002@users.noreply.github.com>
2022-10-28 00:53:34 -0300

    Asistecia me de Octubre - Vilma Jara

--- AsistenciaOctubre/ejercicio-rectangulo.py
+++ AsistenciaOctubre/ejercicio-rectangulo.py
@@ -100,3 +100,33 @@ while True:
         print()
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Alumna Vilma Jara - Asistencia Octubre 2022
+
+class Cubo:
+    """
+    Crear la clase Cubo con los atributos, ancho, alto y profundidad, con
+    un método calcular_volumen que tendrá la formula:
+    volumen = ancho * altura * profundidad
+    que el usuario ingrese los valores.
+    """
+
+    def __init__(self, ancho, altura, profundidad):
+        self.ancho = ancho
+        self.altura = altura
+        self.profundidad = profundidad
+
+    def calcular_volumen(self):
+        return self.ancho * self.altura * self.profundidad
+
+
+ancho = int(input('Digite el ancho del cubo: '))
+altura = int(input('Digite la altura del cubo: '))
+profundidad = int(input('Digite la profundidad del cubo: '))
+
+cubo1 = Cubo(ancho, altura, profundidad)
+print(f'El volumen del cubo es: {cubo1.calcular_volumen()}')
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
