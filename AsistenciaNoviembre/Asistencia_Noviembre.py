ef48ab6da32f90bbe2280ec5b158f4bbfef267a6
damianqnz <79475339+damianqnz@users.noreply.github.com>
2022-12-07 18:12:21 -0300

    Update Asistencia_Noviembre.py

--- AsistenciaNoviembre/Asistencia_Noviembre.py
+++ AsistenciaNoviembre/Asistencia_Noviembre.py
@@ -156,3 +156,44 @@ b = [6, 7, 8, 9]
 print(a + b)
 
 # miObjeto1 + miObjeto2 = esto no se podría hacer
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Alumno: Nelson Damian Quiñonez
+# Leccion 11 - Clase 15 POO Parte 8 Diseño de clases y Sobrecarga de Operadores
+
+class Producto:
+    contador_productos = 0  # Variable de clase
+
+    def __init__(self, nombre, precio):
+        Producto.contador_productos += 1  # Aumento para la variable de clase
+        self._id_producto = Producto.contador_productos  # Asignación desde la variable de clase
+        self._nombre = nombre
+        self._precio = precio
+
+    # Métodos setters and getters
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def precio(self):
+        return self._precio
+
+    @precio.setter
+    def precio(self, precio):
+        self._precio = precio
+
+    # Sobre escribimos el método str
+    def __str__(self):
+        return f'Id Producto: {self._id_producto}, Nombre: {self._nombre}, Precio: {self._precio}'
+
+
+if __name__ == '__main__':  # Solo será visible se la prueba se ejecuta desde aquí
+    producto1 = Producto('Camiseta', 100.00)
+    print(producto1)
+    producto2 = Producto('Pantalon', 150.00)
+    print(producto2)
