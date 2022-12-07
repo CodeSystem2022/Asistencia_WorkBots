5eb6ec56553878fa2332a93692ed0948ad1e0ce3
Vilma Jara <105697175+ivi2002@users.noreply.github.com>
2022-12-07 01:12:20 -0300

    Asistecia de Noviembre Vilma Jara

--- Asistencia_Noviembre.py
+++ Asistencia_Noviembre.py
@@ -40,3 +40,43 @@ print(a + b)
 
 # miObjeto1 + miObjeto2 = esto no se puede hacer
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Alumna: Vilma Jara
+
+class Producto:
+    contador_productos = 0  # Variable de clase
+
+    def __init__(self, nombre, precio):
+        Producto.contador_productos += 1  # Aumento para la variable de clase
+        self._id_producto = Producto.contador_productos  # Asignación desde la variable de clase
+        self._nombre = nombre
+        self._precio = precio
+
+    # Métodos setters and getters
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def precio(self):
+        return self._precio
+
+    @precio.setter
+    def precio(self, precio):
+        self._precio = precio
+
+    # Sobre escribimos el método str
+    def __str__(self):
+        return f'Id Producto: {self._id_producto}, Nombre: {self._nombre}, Precio: {self._precio}'
+
+
+if __name__ == '__main__':  # Solo será visible se la prueba se ejecuta desde aquí
+    producto1 = Producto('Camiseta', 100.00)
+    print(producto1)
+    producto2 = Producto('Pantalon', 150.00)
+    print(producto2)
