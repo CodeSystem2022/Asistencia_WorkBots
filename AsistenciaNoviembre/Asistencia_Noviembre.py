219f12af6015de3850331ec7ca8432005a8a0906
FacuLC <112597189+FacuLC@users.noreply.github.com>
2022-12-08 22:17:11 -0300

    Update Asistencia_Noviembre.py
    Listo Grupo WorkBots

--- AsistenciaNoviembre/Asistencia_Noviembre.py
+++ AsistenciaNoviembre/Asistencia_Noviembre.py
@@ -34,6 +34,7 @@ a = "Hola "
 b = "Mundo"
 print(a + b)
 
+
 a = [3, 4, 5]
 b = [6, 7, 8, 9]
 print(a + b)
@@ -243,3 +244,39 @@ if __name__ == '__main__':  # Solo será visible se la prueba se ejecuta desde a
     print(producto1)
     producto2 = Producto('Pantalon', 150.00)
     print(producto2)
+    
+#------------------------------------------------------------------------------------------------------------------------------------------
+    
+  # Leccion 12 - Clase 15 POO Parte 8 Diseño de clases y Sobrecarga de Operadores
+
+  # Alumno: FACUNDO LEONEL CANO
+
+class Persona:
+    def __init__(self, Nombre, Edad, ComidaFavorita):
+        self.Nombre = Nombre
+        self.Edad = Edad
+        self.ComidaFavorita = ComidaFavorita
+
+    def __add__(self, other):  # Metodo de la (Suma, Otro - Other)
+        return f"{self.Nombre} {other.Nombre} {self.ComidaFavorita} {other.ComidaFavorita}"
+
+    def __sub__(self, Otro):  # Metodo de la Resta
+        return self.Edad - Otro.Edad
+
+    
+  # Ingreso de Datos
+
+Persona1 = Persona("Facundo", 21, ["Sandia", "Naranja"])
+
+Persona2 = Persona("Loenel", 18, ["Granada", "Mandarina"])
+
+  # Persona1.__add__(Persona2) Sintaxis Interna y Automatica
+
+print(f'Datos Importantes'.center(50, '-'))
+
+print(f'Suma de los Objetos: {Persona1 + Persona2}')
+
+print(f'Resta de los Objetos: {Persona1 - Persona2}')
+
+print(f'Suma de las Comidas Favoritas: {Persona1.ComidaFavorita + Persona2.ComidaFavorita}')
+
